af5aa0b5635b893bace28943480d54795601ce03
Noah Williamsson <noah.williamsson@gmail.com>
2009-06-16 23:20:32 +0200

    Drop site-specific Google Analytics code

--- index.php
+++ index.php
@@ -516,19 +516,5 @@ table.jobs tr td:first-child {
 </div>
 <!-- wrapper: }}} -->
 
-
-<!-- analytics: {{{ -->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-706612-3");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
-<!-- analytics: }}} -->
-
-
 </body>
 </html>
